777d907c938fd1bc50d9f5480348782f389ce0f3
lizhen <lizhen9291@gmail.com>
2016-12-05 15:07:27 +0800

    add deviceUtil and MD5

---
+++ Utils/src/main/java/com/android/open/utils/DeviceUtil.java
@@ -0,0 +1,90 @@
+package com.android.open.utils;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.util.DisplayMetrics;
+
+import java.util.UUID;
+import java.util.regex.Pattern;
+
+/**
+ * Created by lizhen on 2016/12/5.
+ */
+
+public class DeviceUtil {
+    /**
+     * 获取手机分辨率 480*800的格式输出
+     * @param context
+     * @return
+     */
+    public static String getDeviceDisplayMetrics(Context context) {
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return String.valueOf(displayMetrics.widthPixels).concat("*")
+                .concat(String.valueOf(displayMetrics.heightPixels));
+    }
+
+    /**
+     * 获取屏幕的宽度
+     * @param context
+     * @return
+     */
+    public static int getScreenWidth(Context context){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return displayMetrics.widthPixels;
+    }
+
+    /**
+     * 获取屏幕的高度
+     * @param context
+     * @return
+     */
+    public static int getScreenHeight(Context context){
+        DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+        return displayMetrics.heightPixels;
+    }
+
+    /** UA信息，设备型号 */
+    public static String getProductType() {
+        return android.os.Build.MODEL;
+    }
+
+    /** 生产厂商 */
+    public static String getManufacturer() {
+        return android.os.Build.MANUFACTURER;
+    }
+
+    /** 系统版本 */
+    public static String getSystemVersion() {
+        return android.os.Build.VERSION.RELEASE;
+    }
+
+    /**
+     * 获取设备deviceid
+     * @param ctx
+     * @return imei
+     */
+    public static String getDeviceId(Context ctx){
+
+        String udid = PreferencesUtil.get("udid","");
+        String uniqueId ;
+        if(!TextUtils.isEmpty(udid)){
+            uniqueId = udid;
+        }else{
+            final TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
+            uniqueId = tm.getDeviceId();
+            boolean isAllZero = false;
+            if(uniqueId != null){
+                Pattern pattern = Pattern.compile("[0]*");
+                isAllZero = pattern.matcher(uniqueId).matches();
+            }
+            if(TextUtils.isEmpty(uniqueId) || isAllZero){
+                uniqueId = MD5.crypt(System.currentTimeMillis() + UUID.randomUUID().toString());
+            }
+            PreferencesUtil.put("udid", uniqueId);
+        }
+        return uniqueId;
+
+    }
+
+}
---
+++ Utils/src/main/java/com/android/open/utils/MD5.java
@@ -0,0 +1,131 @@
+package com.android.open.utils;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.os.Looper;
+
+public class MD5 {
+	public static String crypt(String str) {
+		if (str == null || str.length() == 0) {
+			throw new IllegalArgumentException("String to encript cannot be null or zero length");
+		}
+
+		StringBuffer hexString = new StringBuffer();
+
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(str.getBytes());
+			byte[] hash = md.digest();
+
+			for (int i = 0; i < hash.length; i++) {
+				if ((0xff & hash[i]) < 0x10) {
+					hexString.append("0" + Integer.toHexString((0xFF & hash[i])));
+				} else {
+					hexString.append(Integer.toHexString(0xFF & hash[i]));
+				}
+			}
+		} catch (NoSuchAlgorithmException e) {
+			return "";
+		}
+
+		return hexString.toString();
+	}
+
+	public static String getMD5(byte[] source) {
+		String s = null;
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(source);
+			byte tmp[] = md.digest();
+
+			char str[] = new char[16 * 2];
+
+			int k = 0;
+			for (int i = 0; i < 16; i++) {
+				byte byte0 = tmp[i];
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			s = new String(str);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return s;
+	}
+
+	private static final char HEX_DIGITS[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+
+	public static String toHexString(byte[] b) {
+		StringBuilder sb = new StringBuilder(b.length * 2);
+		for (int i = 0; i < b.length; i++) {
+			sb.append(HEX_DIGITS[(b[i] & 0xf0) >>> 4]);
+			sb.append(HEX_DIGITS[b[i] & 0x0f]);
+		}
+		return sb.toString();
+	}
+
+	// 适合大点文件MD5
+	public static String checkSum(String fileName) {
+		if (fileName == null) {
+			return null;
+		}
+		InputStream fis;
+		byte[] buffer = new byte[1024];
+		int numRead = 0;
+		MessageDigest md5;
+		try {
+			fis = new FileInputStream(fileName);
+			md5 = MessageDigest.getInstance("MD5");
+			while ((numRead = fis.read(buffer)) > 0) {
+				md5.update(buffer, 0, numRead);
+			}
+			fis.close();
+			return toHexString(md5.digest());
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/**
+	 * weichar 签名
+	 * @param key
+	 * @return
+	 */
+	public static String getStringMD5(String key) {
+		String value = null;
+		try {
+			MessageDigest currentAlgorithm = MessageDigest.getInstance("MD5");
+			currentAlgorithm.reset();
+			currentAlgorithm.update(key.getBytes());
+			byte[] hash = currentAlgorithm.digest();
+			String d = "";
+			int usbyte = 0;
+			for (int i = 0; i < hash.length; i += 2) {
+				usbyte = hash[i] & 0xFF;
+				if (usbyte < 16) {
+					d = d + "0" + Integer.toHexString(usbyte);
+				} else {
+					d = d + Integer.toHexString(usbyte);
+				}
+				usbyte = hash[(i + 1)] & 0xFF;
+				if (usbyte < 16) {
+					d = d + "0" + Integer.toHexString(usbyte);
+				} else {
+					d = d + Integer.toHexString(usbyte);
+				}
+			}
+			value = d.trim().toLowerCase();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+			Logger.i("MD5","MD5 algorithm not available.");
+		}
+		return value;
+	}
+}
