b90eacce557e692e2b01c16041a4b59a40f75311
lizhen <lizhen9291@gmail.com>
2016-12-08 17:51:08 +0800

    add BaseFragment

--- Library/src/main/java/com/unbelievable/library/nothero/Fragment/BaseFragment.java
+++ Library/src/main/java/com/unbelievable/library/nothero/Fragment/BaseFragment.java
@@ -7,65 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.unbelievable.nothero.R;
-
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BaseFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BaseFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BaseFragment extends Fragment implements View.OnClickListener{
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    private String mParam1;
-    private String mParam2;
-
+public abstract class BaseFragment extends Fragment implements View.OnClickListener{
+    private View mContentView;
+    private Context mContext;
     private OnFragmentInteractionListener mListener;
 
     public BaseFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BaseFragment.
-     */
-    public static BaseFragment newInstance(String param1, String param2) {
-        BaseFragment fragment = new BaseFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        if (getArguments() != null) {}
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.hello_blank_fragment);
-        return textView;
+        mContentView = inflater.inflate(setLayoutResourceID(),container,false);
+        init();
+        setupView();
+        setupData();
+        return mContentView;
     }
+
+    protected abstract int setLayoutResourceID();
+
+    protected  void init(){}
+
+    protected  void setupView(){}
+
+    protected  void setupData(){}
 
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
@@ -76,6 +50,7 @@ public class BaseFragment extends Fragment implements View.OnClickListener{
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+        this.mContext = context;
         if (context instanceof OnFragmentInteractionListener) {
             mListener = (OnFragmentInteractionListener) context;
         } else {
